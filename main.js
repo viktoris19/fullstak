6ff40fb0a9dff616e63c36a1d766349fdd837a2d
Виктория Осипова <viktoriaosipova50@mail.ru>
2025-09-16 00:10:42 +0300

    feat:исправление синтаксичесикх ошибок

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const dlg = document.getElementById('contactDialog');
 const openBtn = document.getElementById('openDialog');
 const closeBtn = document.getElementById('closeDialog');
 const form = document.getElementById('contactForm');
-const phone = document.getElementById('phone')
+const phone = document.getElementById('phone');
 let lastActive = null;
 
 openBtn.addEventListener('click',() => {
@@ -11,7 +11,7 @@ openBtn.addEventListener('click',() => {
     dlg.querySelector('input,select,textarea,button')?.focus();
 });
 
-closeBtn.addEventListener('click', () => dlg.closest('cancel'));
+closeBtn.addEventListener('click', () => dlg.close('cancel'));
 
 form?.addEventListener('submit', (e) => {
     // 1) Сброс кастомных сообщений
@@ -51,8 +51,8 @@ phone?.addEventListener('input', () => {
     
     const parts = [];
     if (d.length > 0) parts.push('+7');
-    if (d.length > 1) parts.push(' ' + d.slice(1, 4));
-    if (d.length >= 4) parts[parts.length - 1] += ')';
+    if (d.length > 1) parts.push(' (' + d.slice(1, 4)); 
+    if (d.length >= 4) parts[parts.length - 1] += ')';  
     if (d.length >= 5) parts.push(' ' + d.slice(4, 7));
     if (d.length >= 8) parts.push('-' + d.slice(7, 9));
     if (d.length >= 10) parts.push('-' + d.slice(9, 11));
